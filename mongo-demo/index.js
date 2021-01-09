3f772d132ecc0e2d075a019d720f43b5479a8b17
pawel.bak <pawel.bak@vao.pl>
2021-01-09 18:18:49 +0100

    custom validator

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -21,7 +21,15 @@ const courseSchema = new mongoose.Schema({
     enum: ["web", "mobile", "network"],
   },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: "A course should have at least one tag",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
@@ -40,9 +48,9 @@ const createCourse = async () => {
   const course = new Course({
     name: "Angular Course",
     author: "Mosh",
-    tags: ["angular", "frontend"],
+    tags: null,
     isPublished: true,
-    category: "-",
+    category: "web",
     price: 15,
   });
 
